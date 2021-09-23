8835328b3519f7ed93fb861eb2492126f70e6dce
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2021-09-23 09:43:05 +0530

    Recursion : Finding power of a number x

---
+++ Recursion/power.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int power(int x , int n)
+{
+    if(n==0)
+        return 1;   
+    else if (n==1)
+        return x;
+    return x*power(x,n-1);
+}
+int main(){
+    int x, n;
+    cin >> x >> n;
+  
+    cout << power(x, n) << endl;
+    return 0;
+}
